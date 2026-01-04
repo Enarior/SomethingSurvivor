df28b31905b068552a1e23e549150dcbbe0dbec3
Enarior <ehouarn.beriot.oconnell@protonmail.com>
2026-01-04 18:14:55 +0100

    Add movement to +1 sign

--- scripts/plus_one.gd
+++ scripts/plus_one.gd
@@ -1,9 +1,13 @@
 extends Node2D
-
+var velocity  = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$PoofTimer.start()
+	velocity = Vector2(randf_range(-50.0,50.0),-150.0)
+
+func _process(delta) -> void:
+	position += velocity * delta
 
 
 func _on_poof_timer_timeout() -> void:
