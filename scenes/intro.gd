46b28f4dc2abb8c06492a67a53df70025eaf0cf0
ehouarn.beriot.oconnell <ehouarn.beriot.oconnell@protonmail.com>
2026-01-09 18:54:05 +0100

    Addd bubble art and sound effects

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -1,7 +1,21 @@
 extends Control
 
+var children_visible_characters = 0
+var mother_visible_characters = 0
 
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	if $Children/ChildBubble/ChildBubbleText.visible_characters == 0.1:
+		$Children/ChildAudioStreamPlayer.play()
+	
+	if $Mother/MotherBubble/MotherBubbleText.visible_characters == 0.1:
+		$Mother/MotherAudioStreamPlayer.play()
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+		
+		
